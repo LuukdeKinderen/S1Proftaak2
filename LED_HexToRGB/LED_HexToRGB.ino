0df04da7e0c9ed0c9d8b8c6d8b992c2d7b0a76d4
Thomas Joon <Thomas.joon2@gmail.com>
2019-11-27 14:37:10 +0100

    hextToDec
    dit is het eerste gedeelte de string moet nog uit elkaar getrokken worden omgerekend en weer samgevoegd.

---
+++ LED_HexToRGB/LED_HexToRGB.ino
@@ -0,0 +1,30 @@
+// Converting from Hex to Decimal:
+
+// NOTE: This function can handle a positive hex value from 0 - 65,535 (a four digit hex string).
+//       For larger/longer values, change "unsigned int" to "long" in both places.
+
+
+unsigned int hexToDec(String hexString) {
+  
+  String payloadString = "";
+  int nextInt;
+  
+  for (int i = 0; i < hexString.length(); i++) {
+    
+    nextInt = int(hexString.charAt(i));
+    if (nextInt >= 48 && nextInt <= 57) nextInt = map(nextInt, 48, 57, 0, 9);
+    if (nextInt >= 65 && nextInt <= 70) nextInt = map(nextInt, 65, 70, 10, 15);
+    if (nextInt >= 97 && nextInt <= 102) nextInt = map(nextInt, 97, 102, 10, 15);
+    nextInt = constrain(nextInt, 0, 15);
+    
+    payloadString += nextInt.length();
+    payloadString += nextInt;
+    
+  }
+
+
+
+
+  
+  return decValue;
+}
