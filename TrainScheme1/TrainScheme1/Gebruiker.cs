87b8a4544319c35c8351e6c5c6972b4e090d76d8
Thomas Joon <Thomas.joon2@gmail.com>
2019-12-16 11:36:40 +0100

    user class

---
+++ TrainScheme1/TrainScheme1/Gebruiker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainScheme1
+{
+    class user
+    {
+        private int balans;
+        private bool checkIn;
+        private bool checkUit;
+        private string uid;
+
+        public bool inCheck();{
+
+        return incheck;
+
+
+        }
+
+    public bool uitCheck();{
+
+        return uitCheck;
+        }
+
+    private void updateBalans;
+
+    }
+}
+//bedrag ophalen, welke trein, weten waar ingecheckt, weten waar uitgecheckt, bedrag uitrekenen
---
+++ TrainScheme1/TrainScheme1/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainScheme1
+{
+    class User
+    {
+        private double balans;
+        private Station checkIn;
+        private Station checkUit;
+
+        public void InCheck(Station bufferStationIn)
+        {
+            checkIn = bufferStationIn;
+        }
+
+        public void UitCheck(Station bufferStationUit)
+        {
+            
+            checkUit = bufferStationUit;
+
+            int checkinIndex = checkIn.GetRailIndex();
+            int checkuitIndex = checkUit.GetRailIndex();
+            int verschil = 0;
+            if (checkinIndex > checkuitIndex)
+            {
+                verschil = checkinIndex - checkuitIndex;
+            }
+            else
+            {
+                verschil = checkuitIndex - checkinIndex;
+            }
+            balans -= verschil * 0.1;
+
+        }
+
+        private double GetBalans()
+        {
+
+            return placeholder;
+        }
+
+    }
+}
+//bedrag ophalen, welke trein, weten waar ingecheckt, weten waar uitgecheckt, bedrag uitrekenen
